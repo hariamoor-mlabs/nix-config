3bba3469085270a430fb5b3aa6aac3b903f1042a
hariamoor-mlabs <hari@mlabs.city>
2022-06-06 22:35:49 -0400

    Revert "Configure wireguard"
    This reverts commit 35dc6de140d90b6dc565b74c7b16daf665d06b75.

--- configuration.nix
+++ configuration.nix
@@ -6,6 +6,22 @@
     fontconfig.enable = true;
   };
  
+  networking = {
+    firewall.allowedUDPPorts = [ 51820 ];
+    networkmanager.enable = true;
+    wg-quick.interfaces.wg0 = {
+      address = [ "10.2.0.2/32" ];
+      dns = [ "10.2.0.1" ];
+      privateKeyFile = "/etc/privkeys/wireguard.txt";
+      peers = [
+        { publicKey = "nG4IB7yIBmezw4rcpX3SK9UyjjuKjIZZhssCD26jlhM=";
+	  allowedIPs = [ "0.0.0.0/0" ];
+	  endpoint = "37.120.244.58:51820"; 
+	}
+      ];
+    };
+  };
+ 
   nix = {
     binaryCaches = [
       "https://hydra.iohk.io/"
